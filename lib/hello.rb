7d54d557de376e17f4901dccb1fdb0ef107d6853
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-04-02 21:00:34 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,13 +8,10 @@ def hello_t(array)
     end
 
     array
-<<<<<<< HEAD
   else
-=======
-  else 
->>>>>>> 8cf595832076875403479e6877afba4b0bdf64cb
     puts "Hey! No block was given!"
   end
 end
+
 
 # call your method here!
